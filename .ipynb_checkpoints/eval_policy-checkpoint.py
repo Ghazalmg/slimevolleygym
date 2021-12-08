6f80245576a55cf7ca0bfc6ef17b41c88a651ba7
Ghazalmg <92544561+Ghazalmg@users.noreply.github.com>
2021-12-08 15:51:11 -0800

    last

--- .ipynb_checkpoints/eval_policy-checkpoint.py
+++ .ipynb_checkpoints/eval_policy-checkpoint.py
@@ -3,6 +3,7 @@ import torch
 import slimevolleygym
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 def binary(x, bits):
     mask = 2 ** torch.arange(bits - 1, -1, -1).to(x.device, x.dtype)
     return x.unsqueeze(-1).bitwise_and(mask).ne(0).byte()
@@ -19,6 +20,8 @@ def eval_policy(policy1,policy2, env='SlimeVolley-v0', num_test_episodes=10, ren
             state = torch.tensor([state], device=device, dtype=torch.float32)
             action1 = binary(policy1.select_action(state),3)
             action2 = binary(policy2.select_action(state),3)
+            print(action1)
+            print(action2)
             next_state, reward, done, _ = test_env.step(action1,action2)
             
             if render:
@@ -59,3 +62,9 @@ if __name__ == "__main__":
     env.close()
 
     eval_policy(policy1=model1,policy2=model2, env=args.env, render=True, verbose=True)
+
+    
+    
+    
+    
+    
