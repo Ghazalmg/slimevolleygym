b71e846493f72c91a1aec6401bb10a3e5d120084
Yaphetsf75 <36113850+Yaphetsf75@users.noreply.github.com>
2021-12-08 16:39:21 -0800

    Untitled

--- eval_policy.py
+++ eval_policy.py
@@ -20,8 +20,8 @@ def eval_policy(policy1,policy2, env='SlimeVolley-v0', num_test_episodes=10, ren
             state = torch.tensor(state).float()
             action1 = binary(policy1.select_action(state),3)
             action2 = binary(policy2.select_action(state),3)
-            print(action1)
-            print(action2)
+            #print(action1)
+            #print(action2)
             next_state, reward, done, _ = test_env.step(action1,action2)
             
             if render:
